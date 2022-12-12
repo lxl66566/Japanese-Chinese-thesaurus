7d971121bb277a290f6cdee027256f64daa2479c
lxl66566 <18259734087@163.com>
2022-12-12 13:31:39 +0800

    rm change.py

--- change.py
+++
@@ -1,6 +0,0 @@
-import json
-s = {}
-with open('nihonngo_for_final.json','r',encoding='utf-8') as f:
-    s = json.load(f)
-with open('nihonngo_for_final.json','w',encoding='utf-8') as f:
-    json.dump(s,f,indent = 4,sort_keys = True,ensure_ascii=False)
