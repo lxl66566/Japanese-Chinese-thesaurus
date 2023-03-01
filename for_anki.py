918794393dbb3f003b3b2d181f33650744a1646f
lxl66566 <18259734087@163.com>
2023-03-01 22:56:12 +0800

    导入 anki 的处理

---
+++ for_anki.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+import json
+out = []
+with open("./final.json", "r", encoding="utf-8") as f:
+    dic = json.load(f)
+    for word, meaning in dic.items():
+        out.append("\t".join([word, meaning]))
+Path.write_text(Path("./final.txt"), "\n".join(out), encoding="utf-8")
